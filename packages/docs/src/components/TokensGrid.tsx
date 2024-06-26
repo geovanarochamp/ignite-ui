e63550b8f546135ed58eabe04f297db1e3f58f8d
geovanarochamp <geovana.rocha.martins@gmail.com>
2024-06-26 10:55:54 -0300

    add tokens doc

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/tokens-grid.css'
+
+interface TokensGridPorps {
+  tokens: Record<string, string>
+  hasRemValue?: boolean
+}
+
+export function TokensGrid({ tokens, hasRemValue }: TokensGridPorps) {
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <th>Name</th>
+        <th>Value</th>
+        {hasRemValue && <th>Pixels</th>}
+      </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+              {hasRemValue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
